9e4dd431a3463a6b534e4d490fd08f11fee0f8b7
glebishead <glebaskayumov@yandex.ru>
2023-01-07 13:25:45 +0200

    Moved the image upload functions to a separate file

---
+++ code/funcs.py
@@ -0,0 +1,25 @@
+import os
+import pygame
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('../data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
+def load_level(filename):
+    filename = "../data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
--- code/main.py
+++ code/main.py
@@ -1,21 +1,8 @@
 import pygame
-import os
 import sys
 
 
-def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Не удаётся загрузить:', name)
-        raise SystemExit(message)
-    image = image.convert_alpha()
-    if color_key is not None:
-        if color_key is -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    return image
+from funcs import load_image, load_level
 
 
 pygame.init()
@@ -120,14 +107,6 @@ def start_screen():
                 return
         pygame.display.flip()
         clock.tick(FPS)
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
 
 
 def generate_level(level):
