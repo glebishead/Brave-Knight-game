68f6051617fb46e9c24ec898f59b00140e4ced5e
glebishead <glebaskayumov@yandex.ru>
2023-01-14 15:29:03 +0200

    Fixed character animation

--- code/main.py
+++ code/main.py
@@ -5,12 +5,11 @@ from funcs import load_image
 from start import start_screen
 from sprites import hero_group, Player
 
-
 pygame.init()
 screen_size = (800, 500)
 screen = pygame.display.set_mode(screen_size)
 FPS = 50
-speed = 1.5
+speed = 1.25
 
 icon = load_image('icon.png')
 
@@ -21,47 +20,47 @@ player = Player(load_image('hero_sheet.png'), 6, 2, 300, 400)
 running = True
 clock = pygame.time.Clock()
 
-
 start_screen()
 
 
 def main_screen():
-    global running
-    fon = pygame.transform.scale(load_image('lvl1.png'), screen_size)
-    screen.blit(fon, (0, 0))
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            player.direction = 'up'
-            player.is_moving = True
-        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            player.direction = 'down'
-            player.is_moving = True
-        
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            player.direction = 'left'
-            player.is_moving = True
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            player.direction = 'right'
-            player.is_moving = True
-        
-        player.update()
-        player.move()
-        if player.location == 1:
-            fon = pygame.transform.scale(load_image('lvl1.png'), screen_size)
-        if player.location == 2:
-            fon = pygame.transform.scale(load_image('lvl2.png'), screen_size)
-        if player.location == 3:
-            fon = pygame.transform.scale(load_image('lvl3.png'), screen_size)
-        screen.blit(fon, (0, 0))
-        hero_group.draw(screen)
-        clock.tick(FPS)
-        pygame.display.flip()
-    pygame.quit()
-    sys.exit()
+	global running
+	fon = pygame.transform.scale(load_image('lvl1.png'), screen_size)
+	screen.blit(fon, (0, 0))
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_UP] or keys[pygame.K_w]:
+			player.direction = 'up'
+			player.is_moving = True
+		elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+			player.direction = 'down'
+			player.is_moving = True
+		
+		if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+			player.direction = 'left'
+			player.is_moving = True
+			player.flag_le = True
+		elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+			player.direction = 'right'
+			player.is_moving = True
+			player.flag_le = False
+		
+		if player.location == 1:
+			fon = pygame.transform.scale(load_image('lvl1.png'), screen_size)
+		if player.location == 2:
+			fon = pygame.transform.scale(load_image('lvl2.png'), screen_size)
+		if player.location == 3:
+			fon = pygame.transform.scale(load_image('lvl3.png'), screen_size)
+		screen.blit(fon, (0, 0))
+		hero_group.draw(screen)
+		player.update()
+		pygame.display.flip()
+		clock.tick(FPS)
+	pygame.quit()
+	sys.exit()
 
 
 main_screen()
--- code/sprites.py
+++ code/sprites.py
@@ -4,103 +4,106 @@ from funcs import load_image
 
 
 class SpriteGroup(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-
-    def get_event(self, event):
-        for sprite in self:
-            sprite.get_event(event)
+	def __init__(self):
+		super().__init__()
+	
+	def get_event(self, event):
+		for sprite in self:
+			sprite.get_event(event)
 
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, group):
-        super().__init__(group)
-        self.rect = None
-
-    def get_event(self, event):
-        pass
+	def __init__(self, group):
+		super().__init__(group)
+		self.rect = None
+	
+	def get_event(self, event):
+		pass
 
 
 class Player(Sprite):
-    def __init__(self, sheet, columns, rows, pos_x, pos_y, start_location=1):
-        super().__init__(hero_group)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows, pos_x, pos_y)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.t = pygame.time.get_ticks()
-        self.pos = (pos_x, pos_y)
-        from main import speed
-        self.speed = speed
-        self.is_moving = False
-        self.direction = None
-        self.location = start_location
-    
-    def cut_sheet(self, sheet, columns, rows, x, y):
-        self.rect = pygame.Rect(x, y, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(pygame.transform.scale(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)), (30, 30)))
-    
-    def update(self):
-        if self.is_moving:
-            if self.direction is not None:
-                self.cur_frame = self.cur_frame + 0.25
-            if self.cur_frame > 11:
-                self.cur_frame = 1
-            if self.cur_frame == 3:
-                self.cur_frame += 2
-            self.image = self.frames[int(self.cur_frame)]
-            if self.direction == 'left':
-                self.image = pygame.transform.flip(self.image, True, False)
-        else:
-            if self.direction == 'left':
-                self.image = self.frames[0]
-        self.t = pygame.time.get_ticks()
+	def __init__(self, sheet, columns, rows, pos_x, pos_y, start_location=1):
+		super().__init__(hero_group)
+		self.frames = []
+		self.cut_sheet(sheet, columns, rows, pos_x, pos_y)
+		self.cur_frame = 0
+		self.image = self.frames[self.cur_frame]
+		self.t = pygame.time.get_ticks()
+		self.pos = (pos_x, pos_y)
+		from main import speed
+		self.speed = speed
+		self.is_moving = False
+		self.direction = None
+		self.flag_le = False
+		self.location = start_location
+	
+	def cut_sheet(self, sheet, columns, rows, x, y):
+		self.rect = pygame.Rect(-10, -100, sheet.get_width() // columns,
+		                        sheet.get_height() // rows)
+		for j in range(rows):
+			for i in range(columns):
+				frame_location = (self.rect.w * i, self.rect.h * j)
+				self.frames.append(pygame.transform.scale(sheet.subsurface(pygame.Rect(
+					frame_location, self.rect.size)), (30, 30)))
+	
+	def update(self):
+		if self.is_moving:
+			if self.direction is not None:
+				self.cur_frame = self.cur_frame + 0.25
+			if self.cur_frame > 11:
+				self.cur_frame = 3
+			if self.cur_frame == 3:
+				self.cur_frame += 2
+			self.image = self.frames[int(self.cur_frame)]
+			if self.flag_le:
+				self.image = pygame.transform.flip(self.image, True, False)
+		else:
+			self.image = self.frames[0]
+			if self.flag_le:
+				self.image = pygame.transform.flip(self.image, True, False)
+		self.t = pygame.time.get_ticks()
+		self.move()
+	
+	def move(self):
+		x, y = self.pos
+		if self.is_moving:
+			match self.direction:
+				case 'up':
+					y -= self.speed
+				case 'down':
+					y += self.speed
+				case 'left':
+					x -= self.speed
+				case 'right':
+					x += self.speed
+		from main import screen_size
+		if self.location == 1:
+			if x <= 0 and y <= 0:
+				self.location = 2
+				x, y = screen_size[0] - 30, screen_size[1] - 40
+		elif self.location == 2:
+			if x <= 0 and 220 <= y <= 300:
+				self.location = 3
+				x, y = screen_size[0] - 30, screen_size[1] - 220
+			if x + 30 > screen_size[0] and y + 40 > screen_size[1]:
+				self.location = 1
+				x, y = 10, 10
+		elif self.location == 3:
+			if x + 30 >= screen_size[0] and 220 <= y <= 300:
+				self.location = 2
+				x, y = 10, screen_size[1] - 220
+		if x < 0:
+			x = 0
+		elif x > screen_size[0] - 30:
+			x = screen_size[0] - 30
+		if y < 0:
+			y = 0
+		elif y > screen_size[1] - 40:
+			y = screen_size[1] - 40
+		self.direction = None
+		self.pos = x, y
+		self.rect = self.image.get_rect().move(self.pos)
 
-    def move(self):
-        x, y = self.pos
-        if self.is_moving:
-            match self.direction:
-                case 'up':
-                    y -= self.speed
-                case 'down':
-                    y += self.speed
-                case 'left':
-                    x -= self.speed
-                case 'right':
-                    x += self.speed
-        from main import screen_size
-        if self.location == 1:
-            if x <= 0 and y <= 0:
-                self.location = 2
-                x, y = screen_size[0] - 30, screen_size[1] - 40
-        elif self.location == 2:
-            if x <= 0 and 220 <= y <= 300:
-                self.location = 3
-                x, y = screen_size[0] - 30, screen_size[1] - 220
-            if x + 30 > screen_size[0] and y + 40 > screen_size[1]:
-                self.location = 1
-                x, y = 10, 10
-        elif self.location == 3:
-            if x + 30 >= screen_size[0] and 220 <= y <= 300:
-                self.location = 2
-                x, y = 10, screen_size[1] - 220
-        if x < 0:
-            x = 0
-        elif x > screen_size[0] - 30:
-            x = screen_size[0] - 30
-        if y < 0:
-            y = 0
-        elif y > screen_size[1] - 40:
-            y = screen_size[1] - 40
-        self.direction = None
-        self.is_moving = False
-        self.pos = x, y
-        self.rect = self.image.get_rect().move(self.pos)
 
 # class Tree(Sprite):
 #     def __init__(self, x, y):
@@ -109,12 +112,12 @@ class Player(Sprite):
 
 
 class Background(Sprite):
-    def __init__(self, w, h, x, y):
-        super().__init__(btn_bg_group)
-        self.image = load_image('bg_button.png')
-        self.image = pygame.transform.scale(self.image, (w // 3 - 10, h // 10 - 5))
-        self.rect = self.image.get_rect().move(
-            x, y)
+	def __init__(self, w, h, x, y):
+		super().__init__(btn_bg_group)
+		self.image = load_image('bg_button.png')
+		self.image = pygame.transform.scale(self.image, (w // 3 - 10, h // 10 - 5))
+		self.rect = self.image.get_rect().move(
+			x, y)
 
 
 sprite_group = SpriteGroup()
